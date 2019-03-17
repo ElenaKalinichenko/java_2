c7d3f176852a726b0d325f41c7ab6b200c6b74b9
Elena <kroska@ng.ru>
2019-03-18 01:22:30 +0700

    урок 2

---
+++ src/lesson2/MyArraySizeException.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class MyArraySizeException  extends Exception{
+
+    private  int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public MyArraySizeException(String message, int number) {
+        super(message);
+        this.number = number;
+    }
+}
